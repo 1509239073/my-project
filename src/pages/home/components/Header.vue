ca061feb1503c03e3ff5e411be05618589dfbf17
allen.tai <1509239073@qq.com>
2018-10-15 21:27:03 +0800

    iconfont Signed-off-by: allen.tai <1509239073@qq.com>

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,9 @@
 <template>
   <div  class="header">
-	 <div class="header-left">返回</div>
-	 <div class="header-input">输入城市/景点/游玩主题</div>
-	 <div class="header-right">城市</div>
+	 <div class="header-left"><span class="iconfont back-icon">&#xe624;</span></div>
+	 <div class="header-input">
+	 <span class="iconfont">&#xe632;</span>输入城市/景点/游玩主题</div>
+	 <div class="header-right">城市 <span class="iconfont arrow-icon">&#xe64a;</span></div>
   </div>
 </template>
 
@@ -12,21 +13,25 @@ export default {
 }
 </script>
 <style lang="stylus" scpoed>
-
+	@import '~styles/varibles.styl'
 	.header
 		display:flex
 		line-height:.86rem
-		background:#00bad4
+		background:$bgColor
 		color:#fff
 		.header-left
 			width:.64rem
 			float:left
+			.back-icon
+				text-align:center
+				font-size:.4rem
 		.header-input
 			flex:1
 			height:.64rem
 			margin-top:.12rem
 			line-height:.64rem
 			margin-left:.2rem
+			padding-left:.2rem
 			background:#fff
 			border-radius:.1rem
 			color:#ccc
@@ -34,5 +39,8 @@ export default {
 			width:1.24rem
 			float:right
 			text-align:center
+			.arrow-icon
+				font-size:.24rem
+				margin-left:-.04rem
 	
 </style>
