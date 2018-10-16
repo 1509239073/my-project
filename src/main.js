c043049d8e34470e136e871899a2fdbda6ae8a13
allen.tai <1509239073@qq.com>
2018-10-16 22:16:23 +0800

    change

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 import App from './App'
 import router from './router'
 import fastClick from 'fastClick'
@@ -10,6 +12,7 @@ import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper, /* { default global options } */)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="wrapper">
+	<swiper :options="swiperOption" >
+	    <!-- slides -->
+	    <swiper-slide><img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1809/31/da037478f37cf202.jpg_750x200_a02cf862.jpg"></swiper-slide>
+	    <swiper-slide><img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1809/c6/2467595fffc3b302.jpg_750x200_cca13d51.jpg"></swiper-slide>
+	    <swiper-slide><img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1809/ff/1bf7d3a4f6591002.jpg_750x200_40809f2a.jpg"></swiper-slide>
+	    <swiper-slide><img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1809/d4/e298dff35d3af802.jpg_750x200_9b0d56c1.jpg"></swiper-slide>
+	    <swiper-slide><img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1809/31/da037478f37cf202.jpg_750x200_a02cf862.jpg"></swiper-slide>
+	   
+	    <!-- Optional controls -->
+	    <div class="swiper-pagination"  slot="pagination"></div>
+	    <!--<div class="swiper-button-prev" slot="button-prev"></div>
+	    <div class="swiper-button-next" slot="button-next"></div>
+	    <div class="swiper-scrollbar"   slot="scrollbar"></div>-->
+	 </swiper>
+	</div>
+
+  
+</template>
+
+<script>
+export default {
+  name:'HomeSwiper',
+  data(){
+  	return {
+  		swiperOption:{
+  			pagination:'.swiper-pagination',
+  			loop: true
+  		}
+  	}
+  } 
+}
+</script>
+<style lang="stylus" scpoed>
+	.wrapper >>> .swiper-pagination-bullet-active
+		 background: #eee
+	.wrapper
+		overflow:hidden
+		width:100%
+		height:0
+		padding-bottom:26.5% 
+		background:#eee
+		.swiper-img
+			width:100%
+</style>
