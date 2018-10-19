2cddec9eb82407b2ceaac8da6d1395f269d79eb5
taixing <1509239073@qq.com>
2018-10-19 10:17:58 +0800

    swiper-update

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,11 +2,8 @@
 	<div class="wrapper">
 	<swiper :options="swiperOption" >
 	    <!-- slides -->
-	    <swiper-slide><img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1809/31/da037478f37cf202.jpg_750x200_a02cf862.jpg"></swiper-slide>
-	    <swiper-slide><img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1809/c6/2467595fffc3b302.jpg_750x200_cca13d51.jpg"></swiper-slide>
-	    <swiper-slide><img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1809/ff/1bf7d3a4f6591002.jpg_750x200_40809f2a.jpg"></swiper-slide>
-	    <swiper-slide><img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1809/d4/e298dff35d3af802.jpg_750x200_9b0d56c1.jpg"></swiper-slide>
-	    <swiper-slide><img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1809/31/da037478f37cf202.jpg_750x200_a02cf862.jpg"></swiper-slide>
+	    <swiper-slide v-for="item of swiperList" :key="item.id"><img class="swiper-img" :src="item.imgUrl"></swiper-slide>
+	   
 	   
 	    <!-- Optional controls -->
 	    <div class="swiper-pagination"  slot="pagination"></div>
@@ -27,7 +24,23 @@ export default {
   		swiperOption:{
   			pagination:'.swiper-pagination',
   			loop: true
-  		}
+  		},
+      swiperList:[{
+        'id':'0001',
+        'imgUrl':'http://img1.qunarzz.com/piao/fusion/1809/31/da037478f37cf202.jpg_750x200_a02cf862.jpg',
+      },{
+        'id':'0002',
+        'imgUrl':'http://img1.qunarzz.com/piao/fusion/1809/c6/2467595fffc3b302.jpg_750x200_cca13d51.jpg',
+      },{
+        'id':'0003',
+        'imgUrl':'http://img1.qunarzz.com/piao/fusion/1809/ff/1bf7d3a4f6591002.jpg_750x200_40809f2a.jpg',
+      },{
+        'id':'0004',
+        'imgUrl':'http://img1.qunarzz.com/piao/fusion/1809/d4/e298dff35d3af802.jpg_750x200_9b0d56c1.jpg',
+      },{
+        'id':'0005',
+        'imgUrl':'http://img1.qunarzz.com/piao/fusion/1809/31/da037478f37cf202.jpg_750x200_a02cf862.jpg',
+      }]
   	}
   } 
 }
