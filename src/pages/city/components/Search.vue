f0768fe68f11a4b2d90b0ca0533bcb3c3da71522
allen.tai <1509239073@qq.com>
2018-10-29 21:15:36 +0800

    city-search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search">
+  	<input type="text" class="search-input" placeholder="输入城市名或拼音">
+  </div>
+</template>
+<script>
+export default {
+  name: 'CitySearch', 
+  
+}
+</script>
+<style lang="stylus" scpoed>
+	@import '~styles/varibles.styl'
+	.search
+		padding: 0 .1rem
+		height:.72rem
+		background:$bgColor
+		.search-input
+			box-sizing:border-box
+			width:100%
+			height:.62rem
+			line-height:.62rem
+			padding: 0 .1rem
+			text-align:center
+			border-radius:.06rem
+			color:#666
+
+	
+	
+</style>
