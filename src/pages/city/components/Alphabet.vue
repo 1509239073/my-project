49a92c764e380ca3c4205a76a2945c06b8f426ef
allen.tai <1509239073@qq.com>
2018-11-04 21:29:47 +0800

    city-components-touch

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,12 @@
 <template>
 	<ul class="list"  >
-		<li class="item" v-for="(item,key) of cities" :key="key">{{key}}</li>
+		<li class="item" v-for="item of letters" :key="item"
+    @click="handleLetterClick" :ref="item"
+    @touchstart="handleTouchStart"
+    @touchmove="handleTouchMove"
+    @touchend="handleTouchEnd"
+    >{{item}}
+    </li>
 		 
 	</ul>
  
@@ -10,7 +16,47 @@ export default {
   name: 'CityAlphabet',
   props:{
     cities:Object
-  }, 
+  },
+  computed:{
+    letters (){
+      const letters = []
+      for (let i in this.cities){
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  data(){
+    return {
+      touchStatus:false
+    }
+  },
+  methods:{
+    handleLetterClick(e){
+      this.$emit('change',e.target.innerText)
+    },
+    handleTouchStart(){
+        this.touchStatus = true
+    },
+    handleTouchMove(e){
+        if(this.touchStatus){
+            const startY = this.$refs['A'][0].offsetTop
+            const touchY = e.touches[0].clientY - 79
+            const index = Math.floor((touchY - startY)/20)
+            if( index >= 0 && index < this.letters.length){
+                this.$emit('change',this.letters[index])
+            }
+
+
+        }
+
+    },
+    handleTouchEnd(){
+        this.touchStatus = false
+    }
+
+
+  } ,
   
 }
 </script>
